fd5b21eb73f862dbfcf64fff59db681cc7c7c4bf
AhmadAmaireh <a_amayreh@hotmail.com>
2022-06-06 06:08:20 +0300

    javascript

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,23 @@
 'use strict';
+const LinkedList = require('./lib/linkedlist');
 
-class LinkedList {
+const list = new LinkedList();
 
-}
 
-module.exports = LinkedList;
+
+list.insert(8);
+list.insert(7);
+list.insert(6);
+list.insert(5);
+list.insert(4);
+list.insert(3);
+list.insert(2);
+list.insert(1);  //this is the head
+
+
+console.log(list.toString());
+
+console.log(7,list.includes(7));
+console.log(9,list.includes(9));
+console.log(0,list.includes(0));
+console.log(1,list.includes(1));
